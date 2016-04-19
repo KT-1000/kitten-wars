b792c06ab653bb9a00c59b191d2ea459ba97a5ac
Nate Rock <rockhowse@gmail.com>
2016-04-18 23:37:51 -0400

    added in Game.py

--- World.py
+++ World.py
@@ -11,4 +11,21 @@ class Planet:
         self.size = size
         self.cities = cities
 
+        self.grid = []
+
+    def create_planet(self):
+
+        for i in range(self.size):
+            new_col = []
+
+            for j in range(self.size):
+                new_row = []
+
+                new_col.append(new_row)
+
+            self.grid.append(new_col)
+
+    def show_planet(self):
+        for grid_row in self.grid:
+            print grid_row
 
